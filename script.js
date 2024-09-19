fcfa1b85ac2317f0a218c16f15705a470a9bcaaa
matheus <matheus.caposse02@gmail.com>
2024-09-19 11:23:17 -0300

    criando tipos de variaveis

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ console.log(nome3)
 //declaração vazia 
 let disciplina =null;
 console.log(disciplina)
+
+//INTEIRAS
+
+let idade= 18;
+console.log(idade)
+
+//FLUTUANTES OU COM CASAS DECIMAIS
+
+let valor= 6.40;
+console.log(valor)
+
+//STRING(TEXTO)
+
+let texto="fiap"
+console.log(texto)
+
+//BOOLEANOS - VERDADEIRO OU FALSO
+
+let aluno=true
+console.log(aluno)
+
+//ARRAY- CONJUNTO DE INFORMAÇÕES
+let = tecnologias =["java", "python", "html"]
+console.log(tecnologias)
