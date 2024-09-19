912287eaaa9346d3827babf17e601c5ed26ea725
matheus <matheus.caposse02@gmail.com>
2024-09-19 11:33:36 -0300

    verificando o tipo da variavel

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
-// alert("ola mundo-externo")
+// // alert("ola mundo-externo")
 
-// DECLARANDO AS VARIAVEIS
+// // DECLARANDO AS VARIAVEIS
 
-//DECLARAÇÃO COM VALOR
+// //DECLARAÇÃO COM VALOR
 
-var nome = "fiap";
-console.log(nome);
+// var nome = "fiap";
+// console.log(nome);
 
-let nome1="cidade"
-console.log(nome1)
+// let nome1="cidade"
+// console.log(nome1)
 
-const nome2="javascript";
-console.log(nome2)
+// const nome2="javascript";
+// console.log(nome2)
 
-//declaração indefinida
+// //declaração indefinida
 
-let nome3;
-console.log(nome3)
+// let nome3;
+// console.log(nome3)
 
-//declaração vazia 
-let disciplina =null;
-console.log(disciplina)
+// //declaração vazia 
+// let disciplina =null;
+// console.log(disciplina)
 
-//INTEIRAS
+// //INTEIRAS
 
-let idade= 18;
-console.log(idade)
+// let idade= 18;
+// console.log(idade)
 
-//FLUTUANTES OU COM CASAS DECIMAIS
+// //FLUTUANTES OU COM CASAS DECIMAIS
 
-let valor= 6.40;
-console.log(valor)
+// let valor= 6.40;
+// console.log(valor)
 
-//STRING(TEXTO)
+// //STRING(TEXTO)
 
-let texto="fiap"
-console.log(texto)
+// let texto="fiap"
+// console.log(texto)
 
-//BOOLEANOS - VERDADEIRO OU FALSO
+// //BOOLEANOS - VERDADEIRO OU FALSO
 
-let aluno=true
-console.log(aluno)
+// let aluno=true
+// console.log(aluno)
 
-//ARRAY- CONJUNTO DE INFORMAÇÕES
-let = tecnologias =["java", "python", "html"]
-console.log(tecnologias)
+// //ARRAY- CONJUNTO DE INFORMAÇÕES
+// let = tecnologias =["java", "python", "html"]
+// console.log(tecnologias)
+
+//CONVERSÕES
+//converta de float para inteiro
+let numeroFloat= 123.456;
+console.log(parseInt(numeroFloat));
+
+//converta de string para float
+
+let numeroString = "654.321"
+console.log(parseFloat(numeroString));
+
+//converta de numero inteiro para string
+
+let numeroInteiro = 789
+console.log(numeroInteiro.toString())
+
+let alunoAula = true
+
+//Ver o tipo da viriavel
+
+let vertipo = typeof alunoAula
+console.log(vertipo)
